257233c66dfda26eed51e3418c2c72405c8de65a
Kirilloao <kirilltr@list.ru>
2023-02-05 20:02:31 +0100

    3 empty tableView is ready

--- shopApp/ViewControllers/HomeViewController.swift
+++ shopApp/ViewControllers/HomeViewController.swift
@@ -2,71 +2,52 @@
 //  HomeViewController.swift
 //  shopApp
 //
-//  Created by Kirill Taraturin on 04.02.2023.
+//  Created by Kirill Taraturin on 05.02.2023.
 //
 
 import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
     
     var products = Product.getProducts()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.collectionView.register(UINib(nibName: "ProductCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ProductCollectionViewCell")
-        self.collectionView.dataSource = self
-        self.collectionView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-    private func strikeText(strike : String) -> NSMutableAttributedString {
-        
-        let attributeString = NSMutableAttributedString(string: strike)
-        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle,
-        value: NSUnderlineStyle.single.rawValue,
-        range: NSMakeRange(0, attributeString.length))
 
-        return attributeString
-        }
+
 
 }
-extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        var saleProduct: [Product] = []
-        for product in products {
-            if product.onSale == true {
-                saleProduct.append(product)
-            }
-        }
-        return saleProduct.count
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        var products = products
+//        var saleProducts: [Product] = []
+//
+//        for product in products {
+//            if product.onSale == true {
+//                saleProducts.append(product)
+//            }
+//        }
+//        return saleProducts.count
+        return 3
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCollectionViewCell", for: indexPath) as! ProductCollectionViewCell
-        
-        var saleProduct: [Product] = []
-        for product in products {
-            if product.onSale == true {
-                saleProduct.append(product)
-            }
-        }
-        
-        let product = saleProduct[indexPath.item]
-        
-        cell.setupCell(product: product)
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "productsTableViewCell") as? ProductsTableViewCell else { return UITableViewCell() }
         
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        0
-    }
 
+    
+    
 }
