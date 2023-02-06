9a62025647a182c8189bd0519c4c58e19e5f3cbc
Kirilloao <kirilltr@list.ru>
2023-02-06 21:13:32 +0100

    Three type of cell is ready with content is ready, need true content

--- shopApp/TableViewCell.swift
+++ shopApp/TableViewCell.swift
@@ -11,9 +11,12 @@ class TableViewCell: UITableViewCell {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let products = Product.getProducts()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,4 +24,19 @@ class TableViewCell: UITableViewCell {
 
     }
 
+}
+
+extension TableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        products[collectionView.tag].image.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        
+        cell.imageView.image = UIImage(named: String(products[collectionView.tag].image))
+        
+        return cell
+    }
 }
