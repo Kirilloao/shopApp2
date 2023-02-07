fd8ebeb369a3713b090cfbcb9c7ff0ff9a327b83
Kirilloao <kirilltr@list.ru>
2023-02-07 23:34:21 +0100

    CatalogVC ready

---
+++ shopApp/CatalogVC/CatalogTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  CatalogTableViewController.swift
+//  shopApp
+//
+//  Created by Kirill Taraturin on 07.02.2023.
+//
+
+import UIKit
+
+class CatalogTableViewController: UITableViewController {
+
+    let categories = DataStore.shared.categories
+    let allProducts = Product.getProducts()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 200
+
+    }
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "catalog", for: indexPath)
+        let category = categories[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = category
+        content.image = UIImage(named: category.lowercased())
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+//     MARK: UITableViewDelegate
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let catalogListVC = segue.destination as? CategoryListViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        
+        switch categories[indexPath.row] {
+        case "Mac":
+            for mac in allProducts {
+                if mac.category == "Mac" {
+                    catalogListVC.currentProducts.append(mac)
+                    catalogListVC.currentTitle = mac.category
+                }
+            }
+        case "iPhone":
+            for mac in allProducts {
+                if mac.category == "iPhone" {
+                    catalogListVC.currentProducts.append(mac)
+                    catalogListVC.currentTitle = mac.category
+                }
+            }
+        case "iPad":
+            for mac in allProducts {
+                if mac.category == "iPad" {
+                    catalogListVC.currentProducts.append(mac)
+                    catalogListVC.currentTitle = mac.category
+                }
+            }
+        case "Watch":
+            for mac in allProducts {
+                if mac.category == "Watch" {
+                    catalogListVC.currentProducts.append(mac)
+                    catalogListVC.currentTitle = mac.category
+                }
+            }
+        default:
+            for mac in allProducts {
+                if mac.category == "AirPods" {
+                    catalogListVC.currentProducts.append(mac)
+                    catalogListVC.currentTitle = mac.category
+                }
+            }
+        }
+    }
+}
---
+++ shopApp/CatalogVC/CategoryListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CategoryListViewController.swift
+//  shopApp
+//
+//  Created by Kirill Taraturin on 07.02.2023.
+//
+
+import UIKit
+
+class CategoryListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var currentProducts: [Product] = []
+    var currentTitle: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        title = currentTitle
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        currentProducts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath) as! CustomTableViewCell
+        
+        let currentProducts = currentProducts[indexPath.row]
+      
+        cell.productNameLabel.text = currentProducts.model
+        cell.priceLabel.text = "\(currentProducts.price) $"
+        cell.productImageView.image = UIImage(named: currentProducts.image)
+
+            return cell
+        }
+
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 150
+        }
+}
